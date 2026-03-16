05807e72d5c016a0cd84fce95accbd3bcc0f7344
thiagofariasx <thiagofarias.x@gmail.com>
2026-03-16 09:26:57 -0300

    Update robo_mestre.py

--- robo_mestre.py
+++ robo_mestre.py
@@ -55,7 +55,9 @@ def configurar_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222") # Adicione esta linha!
+    
+    # Isso ajuda a evitar que o driver se perca na comunicação interna do servidor
+    chrome_options.add_argument("--disable-browser-side-navigation")
     
     prefs = {
         "download.default_directory": DOWNLOAD_PATH,
@@ -65,9 +67,8 @@ def configurar_driver():
     }
     chrome_options.add_experimental_option("prefs", prefs)
     
-    # Forçamos o uso do binário do Chrome que já vem no GitHub
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-    
+    # No GitHub Actions, basta chamar o webdriver.Chrome() sem passar executáveis, 
+    # pois ele já está pré-instalado no ambiente.
     return webdriver.Chrome(options=chrome_options)
 
 def aguardar_download(timeout=60):
